3c6f8f49b31f1231f4d996a1294d3e3cdf88a846
syehacom <kenshicaster@gmail.com>
2021-12-04 20:54:48 +0900

    master

--- expo/App.js
+++ expo/App.js
@@ -4,91 +4,73 @@ import { GLView } from "expo-gl";
 import { Renderer } from "expo-three";
 import { TweenMax } from "gsap";
 import {
-  AmbientLight,
-  Fog,
+  PointLight,
   GridHelper,
   Mesh,
   PerspectiveCamera,
   Scene,
-  PointLight,
-  SpotLight,
   BoxGeometry,
   MeshLambertMaterial,
 } from "three";
 
 export default function App() {
+  const camera = new PerspectiveCamera(30, 1, 1, 100); // カメラが映し出す設定(視野角, アスペクト比, near, far)
 
-  const sphere = new Mesh();
-  const camera = new PerspectiveCamera(30, 1, 1, 100);
-
+  // カメラの初期座標
   let cameraInitialPositionX = 0;
   let cameraInitialPositionY = 2;
-  let cameraInitialPositionZ = 5;
+  let cameraInitialPositionZ = 7;
 
-  function moveUd(distance) {
-    TweenMax.to(sphere.position, 0.2, {
-      z: sphere.position.z + distance,
-    });
+  // TweenMax.to(何が, 何秒で, { z軸に distance 分移動 })
+  const moveUd = (distance) => {
     TweenMax.to(camera.position, 0.2, {
       z: camera.position.z + distance,
     });
-  }
+  };
 
-  function moveLr(distance) {
-    TweenMax.to(sphere.position, 0.2, {
-      x: sphere.position.x + distance,
-    });
+  //TweenMax.to(何が, 何秒で, { x軸に distance 分移動 })
+  const moveLr = (distance) => {
     TweenMax.to(camera.position, 0.2, {
       x: camera.position.x + distance,
     });
-  }
+  };
 
   return (
     <View style={{ flex: 1 }}>
       <GLView
         style={{ flex: 1 }}
         onContextCreate={async (gl) => {
-          // GL Parameter disruption
+          // 3D空間の準備
           const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
-          // Renderer declaration and set properties
-          const renderer = new Renderer({ gl });
-          renderer.setSize(width, height);
-          renderer.setClearColor("#fff");
-          // Scene declaration, add a fog, and a grid helper to see axes dimensions
-          const scene = new Scene();
-          scene.fog = new Fog("#3A96C4", 1, 10000);
-          scene.add(new GridHelper(10, 10));
-          const geometry = new BoxGeometry(2, 2, 2); // 立方体
-          const material = new MeshLambertMaterial({ color: 0x00ddff }); // 影が表示される
-          cube = new Mesh(geometry, material); // それらをまとめて3Dオブジェクトにします
-          cube.position.set(0, 0, 0);
-          scene.add(cube);
-          // Add all necessary lights
-          const ambientLight = new AmbientLight(0x101010);
-          scene.add(ambientLight);
-          const pointLight = new PointLight(0xffffff, 2, 1000, 1);
-          pointLight.position.set(0, 200, 200);
-          scene.add(pointLight);
-          const spotLight = new SpotLight(0xffffff, 0.5);
-          spotLight.position.set(0, 500, 100);
-          spotLight.lookAt(scene.position);
-          scene.add(spotLight);
-          // Add sphere object instance to our scene
-          scene.add(sphere);
-          // Set camera position and look to sphere
+          const renderer = new Renderer({ gl }); // レンダラーの準備
+          renderer.setSize(width, height); // 3D空間の幅と高さ
+          renderer.setClearColor("white"); // 3D空間の配色
+          const scene = new Scene(); // これが3D空間
+          scene.add(new GridHelper(10, 10)); //グリッドを表示
+
+          // 配置するオブジェクト
+          const geometry = new BoxGeometry(2, 2, 2); // 四角い物体
+          const material = new MeshLambertMaterial({ color: "blue" }); // 物体に光を反射させ色や影を表現する
+          cube = new Mesh(geometry, material); // geometryとmaterialでオブジェクト完成
+          cube.position.set(0, 0, 0); // 配置される座標 (x,y,z)
+          scene.add(cube); // 3D空間に追加
+
+          // 3D空間の光！
+          const pointLight = new PointLight(0xffffff, 2, 1000, 1); //一点からあらゆる方向への光源(色, 光の強さ, 距離, 光の減衰率)
+          pointLight.position.set(0, 200, 200); //配置される座標 (x,y,z)
+          scene.add(pointLight); //3D空間に追加
+
+          // カメラの座標　＝　一人称視点
           camera.position.set(
             cameraInitialPositionX,
             cameraInitialPositionY,
             cameraInitialPositionZ
           );
-
-          camera.lookAt(sphere.position);
-
-          // Render function
+   
           const render = () => {
-            requestAnimationFrame(render);
-            renderer.render(scene, camera);
-            gl.endFrameEXP();
+            requestAnimationFrame(render); // アニメーション　moveUd関数、moveLr関数でカメラ座標が移動
+            renderer.render(scene, camera); // レンダリング
+            gl.endFrameEXP(); // 現在のフレームを表示する準備ができていることをコンテキストに通知するpresent (Expo公式)
           };
           render();
         }}
@@ -99,9 +81,10 @@ export default function App() {
             style={{
               fontSize: 36,
               color: "red",
+              textAlign: "center",
             }}
           >
-            Up
+            前へ
           </Text>
         </Pressable>
         <Pressable onPress={() => moveUd(0.2)}>
@@ -109,9 +92,10 @@ export default function App() {
             style={{
               fontSize: 36,
               color: "red",
+              textAlign: "center",
             }}
           >
-            Down
+            後へ
           </Text>
         </Pressable>
         <Pressable onPress={() => moveLr(-0.2)}>
@@ -119,9 +103,10 @@ export default function App() {
             style={{
               fontSize: 36,
               color: "red",
+              textAlign: "center",
             }}
           >
-            Left
+            左へ
           </Text>
         </Pressable>
         <Pressable onPress={() => moveLr(0.2)}>
@@ -129,9 +114,10 @@ export default function App() {
             style={{
               fontSize: 36,
               color: "red",
+              textAlign: "center",
             }}
           >
-            Right
+            右へ
           </Text>
         </Pressable>
       </View>
